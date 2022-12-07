be71773d501ce859a64419151e4d375599782875
Patrick Sonnerat <patrick.sonnerat@hotmail.com>
2022-12-07 14:16:02 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -80,14 +80,14 @@ sudo apt-get -y install nano
 # Now install Docker
 echo -e "\x1b[37;44mInstalling Docker.                                                                                \x1b[0m"
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 cd /
 if [ ! -s nightscout ] # This will be our working directory
   then
