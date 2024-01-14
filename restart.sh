97e45fcdcdc36bbfaefe8873ba4fc679be3e2546
Patrick Sonnerat <patrick.sonnerat@hotmail.com>
2024-01-14 09:20:53 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -10,7 +10,8 @@
 clear
 echo -e "\x1b[37;44mRestarting Nightscout... please wait                                                              \x1b[0m"
 cd /nightscout
-#docker compose down
+sudo docker compose down
+sudo docker compose pull
 echo -e "\x1b[37;44mNow wait 5 minutes until Nightscout will be available again                                       \x1b[0m"
 nohup sudo docker compose up -d &	# run it in background
 cd /nightscout/NSDockVPS
